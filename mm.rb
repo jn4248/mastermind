122d4d985bbb0826a8cebadc93cefc1be07d8d76
Jason <jnelson2@gmail.com>
2018-05-28 16:06:32 -0400

    Functional 1-player game where user is the breaker/guesser...
    User can play as maker, but no separate AI algorithm to use for
    computer guesses.

--- mm.rb
+++ mm.rb
@@ -1,88 +1,35 @@
 
-# The Tic_Tac_Toe game squares
-class Square
-
-  def initialize(number)
-    @position = number
-    @selected = false
-    @owner = nil
-  end
-
-end
 
 # A player in the game
 class Player
   attr_reader :name
-  attr_accessor :role
+  attr_accessor :role, :winner
 
   def initialize(name)
     @name = name
     @role = ""
+    @winner = false
   end
 
   def guess_code
-    guess = ["_", "_", "_", "_"]
-    options = (1..6).to_a
-    selector = ["first", "second", "third", "fourth"]
-    count = 0
     puts "========================================="
-    4.times do
-      begin
-        puts "#{@name}, please select a number for the #{selector[count]} position (1 through 6):"
-        choice = gets.chomp.strip.to_i
-        unless options.include?(choice)
-          raise ArgumentError.new("Selection was not of the correct format.")
-        end
-      rescue ArgumentError=>e
-        puts "Error: #{e.message}"
-        retry
+    begin
+      puts "#{@name}, please enter your guess:"
+      puts "(a four digit code, using the numbers 1-6, and no spaces. eg: 1234)"
+      choice = gets.chomp.strip
+      unless /^[1-6]{4}$/ =~ choice
+        raise ArgumentError.new("Selection was not of the correct format.")
       end
-      guess[count] = choice
-      puts "#{@name} has chosen #{choice.to_s} for the #{selector[count]} position:"
-      p guess
-      count += 1
+    rescue ArgumentError=>e
+      puts "Error: #{e.message}"
+      retry
     end
+    guess = choice.split("").map { |letter| letter.to_i }
+    # p ["#{@name} has guessed:", guess]
+    puts "#{@name} has guessed:"
+    p guess
     puts "========================================="
     return guess
-  end
-
-  def create_code1
-    code = []
-    4.times do
-      code.push(rand(5)+1)
-    end
-    puts "created code:"
-    p code
-    puts "end of create code"
-    puts ""
-    return code
-  end
-
-  def create_code2
-    code = ["_", "_", "_", "_"]
-    options = (1..6).to_a
-    selector = ["first", "second", "third", "fourth"]
-    count = 0
-    puts "=-----=------=-----=-----=----=------=----=----=-----=----="
-    puts "creating code:"
-    4.times do
-      begin
-        puts "#{@name}, please select a number for the #{selector[count]} code number (1 through 6):"
-        choice = gets.chomp.strip.to_i
-        unless options.include?(choice)
-          raise ArgumentError.new("Selection was not of the correct format.")
-        end
-      rescue ArgumentError=>e
-        puts "Error: #{e.message}"
-        retry
-      end
-      code[count] = choice
-      # puts "#{@name} has chosen #{choice.to_s} for the #{selector[count]} position:"
-      p code
-      count += 1
-    end
-    puts "=-----=------=-----=-----=----=------=----=----=-----=----="
-    return code
   end
 
 end
@@ -90,10 +37,43 @@ end
 # Player operated by a human user
 class User < Player
 
+  def create_code
+    puts "=-----=------=-----=-----=----=------=----=----=-----=----="
+    puts "creating code:"
+    begin
+      puts "#{@name}, please enter the secret code:"
+      puts "(a four digit code, using the numbers 1-6, and no spaces. eg: 1234)"
+      choice = gets.chomp.strip
+      unless /^[1-6]{4}$/ =~ choice
+        raise ArgumentError.new("Selection was not of the correct format.")
+      end
+    rescue ArgumentError=>e
+      puts "Error: #{e.message}"
+      retry
+    end
+    code = choice.split("").map { |letter| letter.to_i }
+    # p ["#{@name} has entered the secret code:", code]
+    puts "#{@name} has entered the secret code:"
+    p code
+    puts "=-----=------=-----=-----=----=------=----=----=-----=----="
+    return code
+  end
+
 end
 
 # A Player automated by the computer
 class Computer < Player
+
+  def create_code
+    puts "=-----=------=-----=-----=----=------=----=----=-----=----="
+    puts "creating code:"
+    code = []
+    4.times { code.push(rand(5)+1) }
+    # p code
+    puts ""
+    puts "=-----=------=-----=-----=----=------=----=----=-----=----="
+    return code
+  end
 
 end
 
@@ -103,7 +83,7 @@ class Mastermind
   def initialize(player_name)
     @player = User.new(player_name)
     @computer = Computer.new("Computer")
-    @game_won = false
+    @game_over = false
     @code = []
     @guesses_made = []
     @guess_analytics = {}
@@ -114,59 +94,51 @@ class Mastermind
   # Initiates the game
   def play_game
     # assume roles
-    # maker = set_maker
-    maker = @computer
+    maker = set_maker
+    # maker = @computer
     breaker = (maker == @player) ? @computer : @player
-    @code = maker.create_code2
-    round = 1
-    until (@game_won == true || round > 4)
+    @code = maker.create_code
+    round = 0
+    until (@game_over == true)
+      round += 1
       puts "Round #{round}"
       current_guess = breaker.guess_code
       @guesses_made.push(current_guess)
       if current_guess == @code
         @game_won = true
+        breaker.winner = true
       else
         @guess_analytics[current_guess] = analyze_guess
         display_guesses
       end
-      round += 1
+      if round == 12
+        @game_over = true
+        maker.winner = true
+      end
     end
-    show_winner
+    show_winner(round, maker, breaker)
   end
 
   def analyze_guess
     puts "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
-    puts "inside analyze guess"
     # Record match type: B = position and value, W = value only, N = none
-    # position_check = Hash[ (0..3).to_a.collect { |item| [item, "N"] } ]
-    # analysis = {}
-    analysis = Hash[ (0..3).to_a.collect { |item| [item, "N"] } ]
-    code_matches = []
-    puts "Creating code_matches:"
-    puts ""
-    # check each guess position against all code values
+    analysis = ["N", "N", "N", "N"]
+    code_matches = []  # [guess position, code position, match type]
+
+    # check each guess position against all code values, and label match type
     @guesses_made.last.each_with_index do |num_guess, index_guess|
-      puts "Matching against: "
-      puts "num_guess: #{num_guess} index_guess: #{index_guess} "
       @code.each_with_index do |num_code, index_code|
-        puts "num_code: #{num_code} index_code: #{index_code}"
         if (index_guess == index_code && num_guess == num_code)
           code_matches.push([index_guess, index_code, "B"])
-          puts "matched B to position: " + index_guess.to_s
         elsif (num_guess == num_code)
           code_matches.push([index_guess, index_code, "W"])
-          puts "matched W to position: " + index_guess.to_s
         end
       end
     end
-    puts ""
-    puts "code_matches:"
-    p code_matches
-    puts ""
 
     matches_b = []
     matches_w = []
-    # for each guess position: add matches to analysis: dont overwrite with W
+    # refine list of matches: prioritize "B" matches, and remaining possible "W"
     code_matches.each do |match|
       position_guess = match[0]
       position_code = match[1]
@@ -174,50 +146,33 @@ class Mastermind
       # skip if "B" match already found at this guess or code position
       unless (match_nested_array_at_subposition?(matches_b, position_guess, 0) ||
               match_nested_array_at_subposition?(matches_b, position_code, 1))
-        puts ""
-        puts "passed match_nested on match:"
-        p match
         case match_type
         when "W"
             matches_w.push(match)
-            puts "W added"
         when "B"
           matches_b.push(match)
-          puts "B added"
           # if "B" match, remove any saved "W" at this guess or code position
           matches_w.each do |w_match|
-            puts "checking old Ws"
-            puts "position_guess = " + position_guess.to_s
-            puts "position_code = " + position_code.to_s
             if (w_match[0] == position_guess || w_match[1] == position_code)
               matches_w.delete(w_match)
-              puts "Deleted W:"
-              p w_match
             end
           end
         end
       end
     end
-    puts "B matches"
-    p matches_b
-    puts ""
-    puts "W matches"
-    p matches_w
-
-    puts ""
-    puts "modifying analysis:"
-    puts ""
-
     # add "W" matches to analysis
-    # Can only have one "W" for any particular guess position
+    # Refine "W" matches and add to analysis (only 1 "W" for any 1 guess position)
     w_guess_positions_assigned = []
     w_code_positions_assigned = []
     matches_w.each do |match|
-      unless (w_guess_positions_assigned.include?(match[0]) ||
-              w_code_positions_assigned.include?(match[1]))
-        analysis[match[0]] = match[2]
-        w_guess_positions_assigned.push(match[0])
-        w_code_positions_assigned.push(match[1])
+      position_guess = match[0]
+      position_code = match[1]
+      match_type = match[2]
+      unless (w_guess_positions_assigned.include?(position_guess) ||
+              w_code_positions_assigned.include?(position_code))
+        analysis[position_guess] = match_type
+        w_guess_positions_assigned.push(position_guess)
+        w_code_positions_assigned.push(position_code)
       end
     end
     #  add "B" matches to analysis
@@ -225,7 +180,6 @@ class Mastermind
     puts ""
     puts "Analysis made:"
     p analysis
-    puts "end analyze_guess"
     puts ""
     puts "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
     return analysis
@@ -248,69 +202,29 @@ class Mastermind
     return found_match
   end
 
-  #
-  # def analyze_guess
-  #   puts "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
-  #   puts "inside analyze guess"
-  #   # Record match type: B = position and value, W = value only, N = none
-  #   # position_check = Hash[ (0..3).to_a.collect { |item| [item, "N"] } ]
-  #   # analysis = {}
-  #   analysis = Hash[ (0..3).to_a.collect { |item| [item, "N"] } ]
-  #   code_matches = {}
-  #   puts "Creating code_matches:"
-  #   puts ""
-  #   # check each guess position against all code values
-  #   @guesses_made.last.each_with_index do |num_guess, index_guess|
-  #     puts "Matching against: "
-  #     puts "num_guess: #{num_guess} index_guess: #{index_guess} "
-  #     @code.each_with_index do |num_code, index_code|
-  #       puts "num_code: #{num_code} index_code: #{index_code}"
-  #       if (index_guess == index_code && num_guess == num_code)
-  #         code_matches[index_guess] = "B"
-  #         puts "matched B to position: " + index_guess.to_s
-  #       elsif (!code_matches.has_key?(index_guess) && num_guess == num_code)
-  #         code_matches[index_guess] = "W"
-  #         puts "matched W to position: " + index_guess.to_s
-  #       end
-  #     end
-  #     puts ""
-  #     puts "modifying analysis:"
-  #     puts ""
-  #     puts "code_matches:"
-  #     p code_matches
-  #     puts ""
-  #     # for each guess position: add matches to analysis: dont overwrite with W
-  #     code_matches.each do |position, match_type|
-  #       puts "Code_matches: checking position: " + position.to_s + " (type: " + match_type + ")"
-  #       if analysis[position] == "N"
-  #         analysis[position] = match_type
-  #         puts "added to analysis(N): " + match_type + " at position: " + position.to_s
-  #       elsif analysis[position] == "W" && match_type == "B"
-  #         analysis[position] = match_type
-  #         puts "added to analysis(W): " + match_type + " at position: " + position.to_s
-  #       end
-  #     end
-  #     code_matches = {}
-  #   end
-  #   puts "Analysis made:"
-  #   p analysis
-  #   puts "end analyze_guess"
-  #   puts ""
-  #   puts "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
-  #   return analysis
-  # end
-
   def display_guesses
     puts "---------------------------------"
     puts "Code is:"
     p @code
-    puts "Guesses thus far:"
+    puts ""
+    puts "All guesses made so far:"
+    puts "Black Match: Correct Number in Correct Position."
+    puts "White Match: Correct Number in Wrong Position."
+    puts ""
     display_array = []
-    @guesses_made.each do |guess|
-      display_array = [guess, "Matches:", @guess_analytics[guess]]
-      puts "guess " + @guesses_made.index(guess).to_s
+    count_b = 0
+    count_w = 0
+    @guesses_made.each_with_index do |guess, index|
+      puts "Guess #{(index + 1).to_s}:"
+      analysis = @guess_analytics[guess]
+      display_array = [guess,
+                      "Black:",
+                      analysis.count("B"),
+                      "White:",
+                      analysis.count("W")]
       p display_array
     end
+    puts ""
     puts "end of guesses list"
     puts "---------------------------------"
   end
@@ -336,23 +250,18 @@ class Mastermind
     return maker
   end
 
-  # Displays the current game board
-  def update_game_display
-
-  end
-
-  # Returns true if there is a winner, and updates the winning player object
-  def check_winner_exists
-
-  end
-
   # Displays the winner
-  def show_winner
-    if @game_won == true
-      puts "#{@player.name} Wins!!"
+  def show_winner(round, maker, breaker)
+    if breaker.winner == true
+      puts "#{breaker.name} wins after #{round} rounds!!"
+    elsif maker.winner == true
+      puts "#{round} rounds have been played, #{maker.name} Wins!!"
     else
-      puts "12 rounds have been played, #{@computer.name} Wins!!"
+      puts "I find it strange that nobody won...perhaps an error..."
     end
+    puts ""
+    puts "The secret code was:"
+    p @code
   end
 
 end
